71d191b44ff977408bdb0a25203edc683ae04b76
Andretreccia <andreabtreccia@gmail.com>
2021-12-21 15:41:43 +0100

    Create movie class
    add into display two objects proprieties

---
+++ index.php
@@ -0,0 +1,34 @@
+ <?php
+    /* - è definita una classe ‘Movie’
+  => all'interno della classe sono dichiarate delle variabili d'istanza
+  => all'interno della classe è definito un costruttore
+  => all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà */
+
+
+    class Movie
+    {
+        public $title;
+        public $genre;
+        public $director;
+        public $duration;
+
+        function __construct($title, $genre, $director, $duration)
+        {
+            $this->title =  $title;
+            $this->genre =  $genre;
+            $this->director = $director;
+            $this->duration = $duration;
+        }
+
+        public function getdirector()
+        {
+            return $this->title;
+        }
+    }
+
+    $movie_1 = new Movie('Le ali della libertà', 'Drama', 'Frank Darabont', 142);
+    $movie_2 = new Movie("Matrix", 'action', 'Bill Pope', 136);
+
+    var_dump($movie_1);
+    var_dump($movie_2);
